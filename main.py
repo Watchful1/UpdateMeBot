0d925ff74112dd594f88a79bf56e1d0552714a2e
Watchful <watchful@watchful.gr>
2016-07-03 02:38:44 -0400

    Mostly testing stuff

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import praw
 import OAuth2Util
 import time
 
-r = praw.Reddit("bot:gr.watchful.subsbot (by /u/Watchful1)")
+r = praw.Reddit("subsbot:gr.watchful.subsbot (by /u/Watchful1)")
 o = OAuth2Util.OAuth2Util(r)
 o.refresh(force=True)
 
-while True:
-    print(r.get_me().comment_karma)
-    time.sleep(30)
+subs = {'Watchful1': {'user': 'thisisredditfacts'}}
+
+for key, val in subs.items():
+    if 'user' in val:
+        user = r.get_redditor(val['user'])
+        for comment in user.get_comments():
+            print(str(comment.score)+': '+comment.body)
+
+# filters
+# user: username of submitter, accepts any string. Accepts multiple
+# type: comment or thread, do not include for both
+# subreddit: subreddit the item was submitted to. Accepts multiple
+# karma: a minimum karma the item has to have
+# age comments
