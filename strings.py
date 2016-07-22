d5ff2b91ad257709a7a9a8e5d900f93501039c32
Watchful1 <Watcher1>
2016-07-21 22:59:02 -0400

    Start on messages

---
+++ strings.py
@@ -0,0 +1,18 @@
+alertMessage = (
+	"UpdateMeBot here!"
+	"\n\n/u/AUTHOR has posted a new thread in /r/SUBREDDIT"
+	"\n\nYou can find it here:"
+	"\n\nLINK"
+) # are you still subscribed? do you want to be?
+
+yourUpdatesMessage = ()
+
+confirmationMessage = ()
+
+couldNotSubscribeMessage = ()
+
+deleteUpdatesConfirmationMessage = ()
+
+deleteAllUpdatesConfirmationMessage = ()
+
+confirmationComment = ()
