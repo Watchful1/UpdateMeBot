bd952ce411e78dd4afe1e57b6a43d45fe056cb38
Watchful1 <watchful@watchful.gr>
2025-11-13 11:29:50 -0800

    Hrmmmm

--- src/classes/notification.py
+++ src/classes/notification.py
@@ -41,7 +41,7 @@ class Notification(Base):
 		if self.submission.title is not None:
 			bldr.append(": ")
 
-			characters_available = 90 - utils.bldr_length(bldr)
+			characters_available = 85 - utils.bldr_length(bldr)
 			if len(self.submission.title) > characters_available:
 				bldr.append(self.submission.title[:characters_available-3])
 				bldr.append("...")
